966efa0bf4b214a59b06b0f67ed441b47f07e24f
Shelley Vohr <shelley.vohr@gmail.com>
2024-08-11 20:12:15 +0200

    refactor: extract logic to ElectronExtensionsRendererClient

--- shell/renderer/extensions/electron_extensions_renderer_client.h
+++ shell/renderer/extensions/electron_extensions_renderer_client.h
@@ -8,14 +8,30 @@
 #include <memory>
 
 #include "extensions/renderer/extensions_renderer_client.h"
+#include "v8/include/v8-local-handle.h"
+
+class GURL;
+
+namespace blink {
+class WebElement;
+class WebFrame;
+class WebURL;
+class WebView;
+}  // namespace blink
 
 namespace content {
 class RenderFrame;
-}
+struct WebPluginInfo;
+}  // namespace content
 
 namespace extensions {
 class Dispatcher;
 }
+
+namespace v8 {
+class Isolate;
+class Object;
+}  // namespace v8
 
 namespace electron {
 
@@ -31,6 +47,9 @@ class ElectronExtensionsRendererClient
   ElectronExtensionsRendererClient& operator=(
       const ElectronExtensionsRendererClient&) = delete;
 
+  // Get the LazyInstance for ElectronExtensionsRendererClient.
+  static ElectronExtensionsRendererClient* GetInstance();
+
   // ExtensionsRendererClient implementation.
   void RenderThreadStarted() override;
   bool IsIncognitoProcess() const override;
@@ -38,6 +57,15 @@ class ElectronExtensionsRendererClient
   extensions::Dispatcher* GetDispatcher() override;
 
   bool AllowPopup();
+
+  static bool MaybeCreateMimeHandlerView(
+      const blink::WebElement& plugin_element,
+      const GURL& resource_url,
+      const std::string& mime_type,
+      const content::WebPluginInfo& plugin_info);
+  v8::Local<v8::Object> GetScriptableObject(
+      const blink::WebElement& plugin_element,
+      v8::Isolate* isolate);
 
   void RunScriptsAtDocumentStart(content::RenderFrame* render_frame);
   void RunScriptsAtDocumentEnd(content::RenderFrame* render_frame);
